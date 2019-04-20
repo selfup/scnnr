0b50b4d4a58b02a3bb1b00ceecd997e0d0eebd37
selfup <boudinot.regis@yahoo.com>
2019-04-19 20:14:23 -0500

    nice error message

--- main.go
+++ main.go
@@ -18,6 +18,24 @@ func main() {
 		directory = os.Args[1]
 		patterns = strings.Split(os.Args[2], ",")
 		keywords = strings.Split(os.Args[3], ",")
+	} else {
+		log.Fatal(`
+    
+      Not enough args!
+
+      scnnr <directory> <extension(s)> <keyword(s)>
+
+      Extension(s) and Keywords(s) can be in csv format
+      
+      snnr <directory> .js,.jsx,.py cache=,cache:
+
+      Example:
+
+      $ go run main.go ./ .md,.markdown cache=,main.go
+      File patterns:  [.md .markdown]
+      Keywords:  [cache= main.go]
+      Files matching pattern(s):  [README.md]
+    `)
 	}
 
 	fmt.Println("File patterns: ", patterns, "\nKeywords: ", keywords)
