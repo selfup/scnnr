1ad657967dec98955bcfff5f8536f4892b1bf146
selfup <boudinot.regis@yahoo.com>
2019-08-14 23:12:08 -0500

    parwalk initial fuction api

---
+++ pkg/parwalk.go
@@ -0,0 +1,9 @@
+package scnnr
+
+// ParWalk is a concurrent/parallel directory walker
+func ParWalk(directory string) ([]string, []string, error) {
+	var goodFiles []string
+	var badFiles []string
+
+	return goodFiles, badFiles, nil
+}
